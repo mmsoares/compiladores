dbcdfc2b6ad28c7cd4cdd1fe83639a8677cc0409
Matheus Maciel Soares <mmsoares@inf.ufrgs.br>
2016-03-29 21:17:11 -0300

    Arrumando a main.c

--- main.c
+++ main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "hash.h"
+#include "y.tab.h"
+#include "lex.yy.h"
  
 extern int isRunning();
 extern int getLineNumber();
 extern void initMe();
 
-int main()
+int main(int argc, char **argv)
 {	
 	initMe();
 
-	while (isRunning())
+	if (argc<2)
 	{
-		yyparse();
+		fprintf(stderr, "ERRO! Para rodar, passe o nome de um arquivo junto.\n");
+		exit (1);
+	}
+	if ((yyin = fopen(argv[1], "r"))==0)
+	{
+		fprintf(stderr, "ERRO! Nao foi possivel abrir este arquivo.\n");
+		exit (1);
 	}
 
+	yyparse();
    	hashPrint();
 
 	return 0;
