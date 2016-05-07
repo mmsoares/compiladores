6a6f9824256cf573c1dd49ec5280884ec8ef4a1c
Matheus Maciel Soares <mmsoares@inf.ufrgs.br>
2016-05-07 16:32:02 -0300

    Arrumando as funções de insert e find do hash

--- hash.c
+++ hash.c
@@ -4,14 +4,10 @@
 #include <stdlib.h>
 #include "hash.h"
 
-//implementação das funções como visto em aula
-
-
-
 void hashInit(void){
 	int i;
 	for(i=0;i<HASH_SIZE;++i)
-		Table[i] = 0;
+		Table[i] = NULL;
 }
 
 int hashAddress(char *text){
@@ -23,20 +19,25 @@ int hashAddress(char *text){
 }
 
 HASH_NODE *hashFind(char *text){
-	int i = 0;
-	HASH_NODE *node;
 	int address = hashAddress(text);
-	for(node = Table[i]; node; node->next)
-		if(strcmp(node->text,text))
+	HASH_NODE *node;
+
+	for(node = Table[address]; node; node->next){
+		if(strcmp(node->text,text)==0){
 			return node;
-	return 0;
+		}
+	}
+
+	return NULL;
 }
 
 HASH_NODE *hashInsert(char *text, int type){
 
 	HASH_NODE *newnode;
-	if(newnode == hashFind(text))
-		return newnode;
+	if(hashFind(text)!= NULL){
+		fprintf(stderr, "Returning without creating\n");
+		return hashFind(text);
+	}
 	int address = hashAddress(text);
 	newnode = (HASH_NODE *) calloc(1,sizeof(HASH_NODE));
 	newnode->type = type;
