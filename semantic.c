75f5e4a67dd61c1539e7c38504446457fc5ac08d
Matheus Maciel Soares <mmsoares@inf.ufrgs.br>
2016-05-08 22:21:55 -0300

    Anotando os tipos na arvore e começando check de parametros em chamada de função

--- semantic.c
+++ semantic.c
@@ -13,6 +13,8 @@ void performSemanticValidations(HASH_NODE* hashmap, ASTREE* syntaxtree) {
 	setDataTypeToVarUsage(syntaxtree);
 	setUndefinedDataTypes(syntaxtree);
 	setDataTypeToVarFather(syntaxtree);
+	checkFunctionParameters(syntaxtree);
+	setTypesToAttrAndOperations(syntaxtree);
 	astreePrint(syntaxtree, 0);
 }
 
@@ -241,6 +243,11 @@ void setUndefinedDataTypes(ASTREE *node) {
 		case AST_COMANDOS:
 		case AST_COMANDO_VAZIO:
 		case AST_OUTPUT:
+		case AST_LISTA_VARIAVEIS:
+		case AST_WHILE:
+		case AST_IF:
+		case AST_IF_ELSE:
+		case AST_INPUT:
 			node->dataType = DT_UNDEFINED;
 			break;		
 		default:
@@ -272,36 +279,188 @@ void setDataTypeToVarFather(ASTREE *node) {
 	}	
 }
 
-int setTypes(ASTREE* node) {
-	int i;
-
-	if(node==0) return DT_UNDEFINED;
-
-//
-
-	int typeSon[MAX_SONS];
-	for(i=0;i<MAX_SONS;i++) {
-		typeSon[i] = setTypes(node->son[i]);
+int getDominantType(int type1, int type2, int isDiv) {
+	if(type1 == DT_INT && type2 == DT_INT) {
+		if(isDiv==1) return DT_REAL;
+		else return DT_INT;
+	}
+	if(type1 == DT_INT && type2 == DT_REAL) {
+		return DT_REAL;
+	}
+	if(type1 == DT_INT && type2 == DT_CHAR) {
+		if(isDiv==1) return DT_REAL;
+		else return DT_INT;
+	}
+	if(type1 == DT_INT && type2 == DT_BOOL) {
+		fprintf(stderr, "Error: erro de tipo entre %d e %d\n", type1, type2);
+		exit(4);
+	}	
+	//
+	if(type1 == DT_REAL && type2 == DT_INT) {
+		return DT_REAL;
+	}
+	if(type1 == DT_REAL && type2 == DT_REAL) {
+		return DT_REAL;
+	}
+	if(type1 == DT_REAL && type2 == DT_CHAR) {
+		return DT_REAL;
+	}
+	if(type1 == DT_REAL && type2 == DT_BOOL) {
+		fprintf(stderr, "Error: erro de tipo entre %d e %d\n", type1, type2);
+		exit(4);
+	}
+	//
+	if(type1 == DT_CHAR && type2 == DT_INT) {
+		if(isDiv==1) return DT_REAL;
+		else return DT_INT;
+	}
+	if(type1 == DT_CHAR && type2 == DT_REAL) {
+		return DT_REAL;
+	}
+	if(type1 == DT_CHAR && type2 == DT_CHAR) {
+		if(isDiv==1) return DT_REAL;
+		else return DT_CHAR;
+	}
+	if(type1 == DT_CHAR && type2 == DT_BOOL) {
+		fprintf(stderr, "Error: erro de tipo entre %d e %d\n", type1, type2);
+		exit(4);
+	}
+	//
+	if(type1 == DT_BOOL && type2 == DT_INT) {
+		fprintf(stderr, "Error: erro de tipo entre %d e %d\n", type1, type2);
+		exit(4);
+	}
+	if(type1 == DT_BOOL && type2 == DT_REAL) {
+		fprintf(stderr, "Error: erro de tipo entre %d e %d\n", type1, type2);
+		exit(4);
+	}
+	if(type1 == DT_BOOL && type2 == DT_CHAR) {
+		fprintf(stderr, "Error: erro de tipo entre %d e %d\n", type1, type2);
+		exit(4);
+	}
+	if(type1 == DT_BOOL && type2 == DT_BOOL) {
+		return DT_BOOL;
 	}
 
-	int type = DT_UNDEFINED;
+	return DT_UNDEFINED;
+}
 
-	for(i=0;i<MAX_SONS; i++) {
-		int typeSons = typeSon[i];
+void checkParameterCompatibility(int type1, int type2){
+	fprintf(stderr, "Checking compatibility between %d and %d\n", type1, type2);
+	if(type1 == DT_BOOL) {
+		if(type2 != DT_BOOL) {
+			fprintf(stderr, "Erro: tipos nao compativeis na chamada de funcao\n");
+			exit(4);
+		}
+	}
+	else {
+		if(type2 == DT_BOOL) {
+			fprintf(stderr, "Erro: tipos nao compativeis na chamada de funcao\n");
+			exit(4);
+		}
+	}
+}
 
-		if(typeSon[i] != DT_UNDEFINED) {
-			if(type == DT_UNDEFINED) {
-				type = typeSons;
+void checkFunctionParameters(ASTREE *node) {
+	if(node==0) return;
+	int i;
+
+	if(node->type == AST_CHAMADA_FUNCAO) {
+		ASTREE *declaration;
+		declaration = node->son[0]->symbol->declaration;
+
+		if(declaration->son[2]){
+			//se a declaração tem só um parametro
+			if(declaration->son[2]->type == AST_PARAMETRO) {
+				if(!node->son[1]) {
+					fprintf(stderr, "Erro: parametros de menos na funcao\n");
+					exit(4);
+				}
+				else if(node->son[1]->type == AST_LISTA_PARAM_CHAMADA) {
+					fprintf(stderr, "Erro: Parametros demais na chamada da funcao\n");
+					exit(4);
+				}
+
+				checkParameterCompatibility(node->son[1]->dataType,declaration->son[2]->son[1]->dataType);
 			}
-			else {
+			//se a declaração tem dois ou mais parametros
+			else if(declaration->son[2]->type == AST_LISTA_PARAMETRO) {
+				ASTREE *declarationParam = declaration->son[2];
+				ASTREE *callParam = node->son[1];
 
+				//if(!callParam->son[0]) exit(4);
+				//checkParameterCompatibility(declarationParam->son[0]->dataType, callParam->son[0]->dataType);
+			}
+		}
+		//se a declaração não tem parametro
+		else {
+			if(node->son[1]) {
+				fprintf(stderr, "Erro: Parametros demais na chamada da funcao\n");
+				exit(4);
 			}
 		}
 	}
 
-	return type;
+	for(i=0;i<MAX_SONS;i++) {
+		checkFunctionParameters(node->son[i]);
+	}		
 }
 
-void checkTypes(ASTREE *node) {
-	
+int setTypesToAttrAndOperations(ASTREE* node) {
+	int i;
+	if(node==0) return DT_UNDEFINED;
+	int type0;
+	int type1;
+
+	if(node->dataType == DT_UNDEFINED || node->type == AST_FUNCAO) {
+		for(i=0;i<MAX_SONS;i++) {
+			setTypesToAttrAndOperations(node->son[i]);
+		}
+	}
+	else if(node->dataType == DT_NOT_SET || !node->dataType || node->dataType == DT_INT || node->dataType == DT_BOOL || node->dataType == DT_CHAR || node->dataType == DT_REAL) {
+		switch(node->type) {
+			case AST_OP_SOMA:
+			case AST_OP_SUB:
+			case AST_OP_MUL:
+			case AST_OP_MENOR:
+			case AST_OP_MAIOR:
+			case AST_OP_LE:
+			case AST_OP_GE:
+			case AST_OP_EQ:
+			case AST_OP_NE:
+			case AST_OP_AND:
+			case AST_OP_OR:
+				 type0 = setTypesToAttrAndOperations(node->son[0]);
+				 type1 = setTypesToAttrAndOperations(node->son[1]);			 
+				 node->dataType = getDominantType(type0, type1, 0);
+				 return node->dataType;
+			case AST_OP_DIV:
+				 type0 = setTypesToAttrAndOperations(node->son[0]);
+				 type1 = setTypesToAttrAndOperations(node->son[1]);			 
+				 node->dataType = getDominantType(type0, type1, 1);
+				 return node->dataType;
+			case AST_ATRIBUICAO:
+				 type0 = node->son[0]->dataType;
+				 type1 = setTypesToAttrAndOperations(node->son[1]);
+				 node->dataType = getDominantType(type0, type1, 0);
+				 return node->dataType;
+	  	 	case AST_ATRIBUICAO_VETOR:
+  	 			type0 = node->son[0]->dataType;
+				type1 = setTypesToAttrAndOperations(node->son[1]);
+				if(type1 == DT_BOOL || type1 == DT_REAL){
+					fprintf(stderr, "Error: erro de tipo no acesso a uma posicao de vetor\n");
+					exit(4);
+				}
+				type1 = setTypesToAttrAndOperations(node->son[2]);
+				node->dataType = getDominantType(type0, type1);
+				return node->dataType;
+			default:
+		 		return node->dataType;
+	  }
+	}
+ 	else{
+	  for(i=0;i<MAX_SONS;i++) {
+			setTypesToAttrAndOperations(node->son[i]);
+	  }  
+ 	}
 }
