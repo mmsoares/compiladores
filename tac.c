7af067272cb0f2261f767ee4288054279e4363cf
Matheus Maciel Soares <mmsoares@inf.ufrgs.br>
2016-05-22 21:45:34 -0300

    Começando as funções do tac

--- tac.c
+++ tac.c
@@ -1,1 +1,131 @@
 #include "tac.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int labelCounter = 0;
+int tempCounter = 0;
+
+TAC_NODE* tac_createNode(int type, HASH_NODE* result, HASH_NODE* operation1, HASH_NODE* operation2) {
+	TAC_NODE* node = 0;
+	node = (TAC_NODE*) calloc(1, sizeof(TAC_NODE));
+	node->type = type;
+	node->result = result;
+	node->operator1 = operation1;
+	node->operator2 = operation2;
+	node->previous = NULL;
+	node->next = NULL;
+	return node;
+}
+
+TAC_NODE* tac_join(TAC_NODE* tac1, TAC_NODE* tac2) {
+	if(tac1==NULL) return tac2;
+	if(tac2==NULL) return tac1;
+
+	TAC_NODE* aux;
+	for(aux = tac2; aux->previous != NULL; aux = aux->previous);   //move o iterador auxiliar até o primeiro elemento do tac
+	aux->previous = tac1;
+	return tac2;
+}
+
+TAC_NODE* generateTacCode(ASTREE* syntaxtree) {
+
+}
+
+TAC_NODE* revertTac(TAC_NODE* tac) {
+	TAC_NODE* aux;
+
+	if(tac==NULL) return tac;
+
+	for (aux = tac; aux->previous != NULL; aux = aux->previous) {
+		aux->previous->next = aux;
+	}
+	return aux;
+}
+
+TAC_NODE* tac_createOperation(int type, TAC_NODE* code0, TAC_NODE* code1) {
+
+}
+
+TAC_NODE* tac_createIfThen(TAC_NODE* code0, TAC_NODE* code1) {
+
+}
+
+TAC_NODE* tac_createIfElse(TAC_NODE* code0, TAC_NODE* code1) {
+
+}
+
+TAC_NODE* tac_createIfLoop(TAC_NODE* code0, TAC_NODE* code1) {
+
+}
+
+TAC_NODE* tac_createVectorInitialization(TAC_NODE* literal, TAC_NODE* next) {
+
+}
+
+TAC_NODE* tac_createAssignVector(HASH_NODE* symbol, TAC_NODE* index, TAC_NODE* attribute) {
+
+}
+
+TAC_NODE* tac_createVectorRead(HASH_NODE* symbol, TAC_NODE* index) {
+
+}
+
+TAC_NODE* tac_createFunctionDeclaration(HASH_NODE* symbol, TAC_NODE* parameter_list, TAC_NODE* statement_list) {
+
+}
+
+TAC_NODE* tac_createFunctionParameters(HASH_NODE* symbol, TAC_NODE* next) {
+
+}
+
+TAC_NODE* tac_createFunctionCall(HASH_NODE* symbol, TAC_NODE* params) {
+
+}
+
+TAC_NODE* tac_createFunctionArguments(HASH_NODE* symbol, TAC_NODE* expression, TAC_NODE* next) {
+
+}
+
+TAC_NODE* tac_createOutput(TAC_NODE* expression, TAC_NODE* next) {
+
+}
+
+void printTacNode(TAC_NODE* tac) {
+	if(tac == NULL) return;
+
+	switch(tac->type) {
+		case TAC_SYMBOL: fprintf(stderr, "TAC_SYMBOL "); break;
+		default: fprintf(stderr, "UNKNOWN %d", tac->type); break;
+	}
+
+	if(tac->result)    fprintf(stderr, "res=[%s] ", tac->result->key)
+	if(tac->operator1) fprintf(stderr, "op1=[%s] ", tac->operator1->key);
+	if(tac->operator2) fprintf(stderr, "op2=[%s] ", tac->operator2->key);
+	fprintf(stderr, "\n");
+}
+
+void printTacList(TAC_NODE* tac) {
+	if(tac != NULL) {
+		printTacNode(tac);
+		printTacList(tac->next);
+	}
+}
+
+void printTacListReverse(TAC_NODE* tac) {
+	if(tac != NULL) {
+		printTacListReverse(tac->previous);
+		printTacNode(tac);
+	}
+}
+
+char* makeLabel() {
+	char label[256];
+	sprintf(label, "Label_%d", labelCounter++);
+	return label;
+}
+
+char* makeTemp() {
+	char temp[256];
+	sprintf(temp, "Temp_%d", tempCounter++);
+	return temp;
+}
--- tac.h
+++ tac.h
@@ -1,5 +1,93 @@
 #ifndef     TAC_HEADER
 #define     TAC_HEADER
 
+#include "hash.h"
+#include "astree.h"
+
+#define TAC_SYMBOL			01
+#define TAC_VECTOR			02
+#define TAC_LITERAL			03
+#define TAC_LABEL			04
+#define TAC_TEMP			05
+#define TAC_POINTER			06
+#define TAC_ADDRESS			07
+
+#define TAC_OP_PLUS			11
+#define TAC_OP_MINUS		12
+#define TAC_OP_TIMES		13
+#define TAC_OP_DIVIDE		14
+#define TAC_OP_MODULE		15
+
+#define TAC_OP_EQUAL		21
+#define TAC_OP_NOTEQUAL		22
+#define TAC_OP_LESSER		23
+#define TAC_OP_GREATER		24
+#define TAC_OP_LESSEREQUAL	25
+#define TAC_OP_GREATEREQUAL	26
+#define TAC_OP_AND			27
+#define TAC_OP_OR			28
+#define TAC_OP_NOT			29
+
+#define TAC_ASSIGN_VAR		31
+#define TAC_JUMP			32
+#define TAC_IF_THEN			33
+#define TAC_IF_ELSE			34
+#define TAC_OUTPUT			35
+#define TAC_INPUT			36
+#define TAC_RETURN			37
+
+#define TAC_WORD			41
+#define TAC_BOOL			42
+#define TAC_BYTE			43
+
+#define TAC_VARIABLE_DECLARATION	51
+
+#define TAC_FUNCTION_DECLARATION	61 
+#define TAC_FUNCTION_PARAMS			62
+#define TAC_FUNCTION_BEGIN			63
+#define TAC_FUNCTION_END			64
+#define TAC_FUNCTION_CALL			65
+#define TAC_FUNCTION_ARGUMENTS		66
+
+#define TAC_VECTOR_DECLARATION		71
+#define TAC_VECTOR_INIT				72
+#define TAC_VECTOR_READ				73
+#define TAC_VECTOR_WRITE			74
+
+#define TAC_POINTER_DECLARATION		82
+
+
+typedef struct tac_node {
+	int type;
+	HASH_NODE* result;
+	HASH_NODE* operator1;
+	HASH_NODE* operator2;
+	struct tac_node* previous;
+	struct tac_node* next;
+} TAC_NODE;
+
+TAC_NODE* createTacNode(int type, HASH_NODE* result, HASH_NODE* operation1, HASH_NODE* operation2);
+TAC_NODE* joinTacs(TAC_NODE* tac1, TAC_NODE* tac2);
+TAC_NODE* generateTacCode(ASTREE* syntaxtree);
+TAC_NODE* revertTac(TAC_NODE* tac);
+TAC_NODE* tac_createOperation(int type, TAC_NODE* code0, TAC_NODE* code1);
+TAC_NODE* tac_createIfThen(TAC_NODE* code0, TAC_NODE* code1);
+TAC_NODE* tac_createIfElse(TAC_NODE* code0, TAC_NODE* code1);
+TAC_NODE* tac_createIfLoop(TAC_NODE* code0, TAC_NODE* code1);
+TAC_NODE* tac_createVectorInitialization(TAC_NODE* literal, TAC_NODE* next);
+TAC_NODE* tac_createAssignVector(HASH_NODE* symbol, TAC_NODE* index, TAC_NODE* attribute);
+TAC_NODE* tac_createVectorRead(HASH_NODE* symbol, TAC_NODE* index);
+TAC_NODE* tac_createFunctionDeclaration(HASH_NODE* symbol, TAC_NODE* parameter_list, TAC_NODE* statement_list);
+TAC_NODE* tac_createFunctionParameters(HASH_NODE* symbol, TAC_NODE* next);
+TAC_NODE* tac_createFunctionCall(HASH_NODE* symbol, TAC_NODE* params);
+TAC_NODE* tac_createFunctionArguments(HASH_NODE* symbol, TAC_NODE* expression, TAC_NODE* next);
+TAC_NODE* tac_createOutput(TAC_NODE* expression, TAC_NODE* next);
+
+void printTacNode(TAC_NODE* tac);
+void printTacList(TAC_NODE* tac);
+void printTacListReverse(TAC_NODE* tac);
+
+char* makeLabel();
+char* makeTemp();
 
 #endif
